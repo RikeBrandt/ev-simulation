602eae7a0296767ad66611814b416d307b27494c
RikeBrandt <brandtrike@web.de>
2025-03-12 11:58:37 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+type SimulationValues = {
+  chargePoints: number;
+  utilizationRate: number;
+  power: number;
+  consumption: number;
+};
+
+function App() {
+  return <></>;
+}
+
+export default App;
